b864373c20b40c1cdd9b50ee710b147c28ee614b
nkp1111 <neerajp4321@gmail.com>
2023-01-13 21:49:05 +0530

    Feat: add function to check old and add new user

--- project-stockchecker/routes/api.js
+++ project-stockchecker/routes/api.js
@@ -1,15 +1,24 @@
 'use strict';
 let axios = require("axios")
+const bcrypt = require("bcrypt")
 let symbols
 let baseUrl = "https://stock-price-checker-proxy.freecodecamp.rocks/v1/stock/"
 let data
 let data2
+let { Like, User } = require("./likeDb")
 
 module.exports = function (app) {
 
   app.route('/api/stock-prices')
     .get(async function (req, res) {
       symbols = req.query.stock
+      const like = req.query.like
+
+      if (like) {
+        const currentUser = req.headers['x-forwarded-for'] || req.socket.remoteAddress
+        checkUser(currentUser)
+      }
+
       // for more than one symbol 
       if (Array.isArray(symbols)) {
         for (let s of symbols) {
@@ -30,23 +39,40 @@ module.exports = function (app) {
 
       if (!data2) {
         // response for one symbol 
-        res.send(
-          {
-            "stockData": {
-              "stock": symbols,
-              "price": data["previousClose"],
-              "rel_likes": 1
-            }
+        res.send({
+          "stockData": {
+            "stock": symbols,
+            "price": data["previousClose"],
+            "likes": 1
           }
-        )
+        })
       } else {
-        // response for two symbol 
+        // response for two symbol
         res.send({
           "stockData": [
-            { "stock": symbols[0], "price": data["previousClose"], "rel_likes": 1 },
-            { "stock": symbols[1], "price": data["previousClose"], "rel_likes": 1 }]
+            { "stock": symbols[0], "price": parseFloat(data["previousClose"]), "rel_likes": 1 },
+            { "stock": symbols[1], "price": parseFloat(data["previousClose"]), "rel_likes": 1 }]
         })
       }
     });
-
 };
+
+function checkUser(user) {
+  // check if current user is old user or new one
+  User.find().then(async data => {
+    let userip = user.split(":").slice(-1,)[0].split(".").join("")
+    let hashedip = await bcrypt.hash(user, 12)
+    console.log(hashedip, data)
+
+    if (data.indexOf(hashedip) !== -1) {
+      return true
+    } else {
+      let newone = new User({
+        user: hashedip
+      })
+      await newone.save()
+      return false
+    }
+  })
+
+}
