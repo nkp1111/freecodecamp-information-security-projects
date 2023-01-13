5db07891aac7dfc6926327c71cea9a62b821d11f
nkp1111 <neerajp4321@gmail.com>
2023-01-13 20:03:57 +0530

    Feat: add functional tests base

--- project-stockchecker/tests/2_functional-tests.js
+++ project-stockchecker/tests/2_functional-tests.js
@@ -5,6 +5,49 @@ const server = require('../server');
 
 chai.use(chaiHttp);
 
-suite('Functional Tests', function() {
+suite('Functional Tests', function () {
+  test("Viewing one stock GET /api/stock-prices/", function (done) {
+    chai.request(server)
+      .get("/api/stock-prices?stock=GOOG")
+      .end(function (err, res) {
+        assert.equal(res.status, 200)
+        done()
+      })
+  })
 
+  test("#2 Viewing one stock and liking it: GET request /api/stock-prices/", function (done) {
+    chai.request(server)
+      .get("/api/stock-prices?stock=GOOG&like=true")
+      .end(function (err, res) {
+        assert.equal(res.status, 200)
+        done()
+      })
+  })
+
+  test("#3 Viewing the same stock and liking it again: GET request /api/stock-prices/", function (done) {
+    chai.request(server)
+      .get("/api/stock-prices?stock=GOOG&like=true")
+      .end(function (err, res) {
+        assert.equal(res.status, 200)
+        done()
+      })
+  })
+
+  test("#4 Viewing two stocks: GET request /api/stock-prices/", function (done) {
+    chai.request(server)
+      .get("/api/stock-prices?stock=GOOG&stock=MSFT")
+      .end(function (err, res) {
+        assert.equal(res.status, 200)
+        done()
+      })
+  })
+
+  test("#5 Viewing two stocks and liking them: GET request /api/stock-prices/", function (done) {
+    chai.request(server)
+      .get("/api/stock-prices?stock=GOOG&stock=MSFT&like=true")
+      .end(function (err, res) {
+        assert.equal(res.status, 200)
+        done()
+      })
+  })
 });
