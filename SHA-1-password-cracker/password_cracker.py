9e0b34da9f22fad2f9c7803d633fceb2eed58c1d
nkp1111 <neerajp4321@gmail.com>
2023-01-19 10:56:59 +0530

    Feat: add password check without salt

--- SHA-1-password-cracker/password_cracker.py
+++ SHA-1-password-cracker/password_cracker.py
@@ -1,4 +1,15 @@
 import hashlib
 
-def crack_sha1_hash(hash):
-    
+
+def crack_sha1_hash(hash, use_salts=False):
+    print(hash, use_salts)
+    with open("top-10000-passwords.txt", mode="r") as password_file:
+        passwords = password_file.readlines()
+        passwords = tuple(p.strip() for p in passwords)
+        for password in passwords:
+            p_crack = hashlib.sha1()
+            p_crack.update(password.encode("utf-8"))
+            cracked_password_hash = p_crack.hexdigest()
+            if cracked_password_hash == hash:
+                print(password)
+                return password
