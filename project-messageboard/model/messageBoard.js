43e185ca76268d2fc242d46c68504faf87199e48
nkp1111 <neerajp4321@gmail.com>
2023-01-21 11:36:17 +0530

    Feat: add model function to get a thread and reply

--- project-messageboard/model/messageBoard.js
+++ project-messageboard/model/messageBoard.js
@@ -58,4 +58,36 @@ const threadSchema = new mongoose.Schema({
 const Thread = mongoose.model("Thread", threadSchema)
 const Reply = mongoose.model("Reply", replySchema)
 
-module.exports = { Thread, Reply }
+
+const getThreadId = async (text, delete_password) => {
+  let thread = await Thread.findOne({ text: text ? text : "test", delete_password: text ? text : "test" })
+  if (!thread) {
+    let thread = await Thread.create({
+      board: "test",
+      text: "test",
+      delete_password: "test",
+      replies: []
+    })
+  }
+
+  return thread
+}
+
+const getReplyId = async (thread_id) => {
+  let thread = await Thread.findById(thread_id)
+  let reply = thread.replies.length > 0 ? thread.replies[0] : ""
+  if (!reply) {
+    let reply = new Reply({
+      text: "test",
+      created_on: new Date(),
+      reported: false,
+      delete_password: "test",
+    })
+    thread.replies.push(reply)
+    thread = await thread.save()
+    reply = thread.replies[0]
+  }
+  return reply
+}
+
+module.exports = { Thread, Reply, getThreadId, getReplyId }
